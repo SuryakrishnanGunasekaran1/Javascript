9c4bb9c898f32189ac24058cd42bbdeb689e7a5e
SuryakrishnanGunasekaran1 <169053660+SuryakrishnanGunasekaran1@users.noreply.github.com>
2024-05-22 11:30:58 +0530

    Add files via upload

---
+++ Day 13/objectdemo.js
@@ -0,0 +1,43 @@
+const movie = {
+    Name : "Godzilla X Kong",
+    Director : "Adam Wingards",
+    Based_on : "Godzilla by Toco Pvt Ltd,Legendary Comics",
+    Genre : "Action,Sci-fi,Thrilling,",
+    Universe : "Monsterverse"
+
+}
+console.log(movie.Name)
+console.log(movie.Based_on)
+console.log(movie.Universe)
+console.log("****************")
+
+Godzilla = {
+    name : "Titanus Gojira",
+    Species : "Titan",
+    Type : "Alpha",
+    Height : "393 feet",
+    weigth : "164000 tons",
+    Powers : {Agility:"18kmph",Energy_yield : "Atomic Breath",},
+    Signature_move : "Tail Swipe,Kiss of Death"
+}
+
+Kong = {
+    name : "Titanus Kong",
+    Species : "Titan",
+    Type : "Alpha",
+    Height : "335 feet",
+    weigth : "12000 tons",
+    Powers : {Agility:"60kmph",Durability:"Stronger than other titans",Master_Combatant:"highly skilled",Intelligence:"surpass humans",Punch :"million newton force",},
+    Signature_move : "Superman Punch"
+}
+console.log(Kong.name)
+console.log(Kong.Type)
+console.log(Kong.weigth)
+console.log(Kong.Powers.Agility)
+console.log(Kong.Powers.Master_Combatant)
+console.log(Kong.Powers.Punch)
+console.log("********************")
+console.log(Godzilla.name)
+console.log(Godzilla.weigth)
+console.log(Godzilla.Powers.Energy_yield)
+console.log(Godzilla.Signature_move)
