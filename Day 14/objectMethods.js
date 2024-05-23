ada6da6702214eda0c6f103c7b2d3e06193a59ce
SuryakrishnanGunasekaran1 <169053660+SuryakrishnanGunasekaran1@users.noreply.github.com>
2024-05-23 13:08:47 +0530

    Add files via upload

---
+++ Day 14/objectMethods.js
@@ -0,0 +1,25 @@
+const Employee = {
+    Ename:"Adam",
+    Eid : 8890,
+    Email : "adam@gmail.com",
+    Design : "TeamLead",
+    Branch :"Banglore"
+}
+
+let emp1 = Employee
+//freeze method
+Object.freeze(emp1)
+emp1.Ename = "bolt"
+console.log(emp1)
+// assign method
+Object.assign(emp1)
+Ename = "Clint"
+Design = "HR"
+console.log(Ename)
+console.log(emp1)
+//entries method
+let x = Object.entries(emp1)
+//fromEntries method
+console.log(x)
+ y = Object.fromEntries(x)
+console.log(y)
