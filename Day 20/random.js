4a5da5485df74193a15dd84942feb83d4dff382f
SuryakrishnanGunasekaran1 <169053660+SuryakrishnanGunasekaran1@users.noreply.github.com>
2024-05-30 16:56:00 +0530

    Update random.js

--- Day 20/random.js
+++ Day 20/random.js
@@ -1,2 +1,17 @@
-let r = Math.floor(Math.random()*10)
-console.log(r)
+const randomNumber = Math.floor(Math.random() * 100) ;
+
+        function checkGuess() {
+            const userGuess =25
+            
+            if (userGuess == randomNumber) {
+                console.log('Congratulations! You guessed the correct number!');
+                
+            } else if (userGuess < randomNumber) {
+                console.log(`you entered ${randomNumber} Too high! Try again.`);
+              
+            } else if(userGuess > randomNumber){
+                console.log(`you entered ${randomNumber} Too low! Try again.`);
+              
+            }
+        }
+        checkGuess();
